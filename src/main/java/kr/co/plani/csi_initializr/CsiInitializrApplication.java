a247917ca524ce6e4c0a76f0a179d45cbcbcd979
권다애 <kwondarc@plani.co.kr>
2023-04-26 15:42:28 +0900

    Initial commit

---
+++ src/main/java/kr/co/plani/csi_initializr/CsiInitializrApplication.java
@@ -0,0 +1,13 @@
+package kr.co.plani.csi_initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsiInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsiInitializrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/plani/csi_initializr/CsiInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.plani.csi_initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsiInitializrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
